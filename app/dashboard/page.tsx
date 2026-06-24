0a159d06c480d29142f9cc5774a2fae0e0bcbc74
Kenny <oluyoleprovidence@gmail.com>
2026-06-24 15:32:13 +0100

    create the sales chart

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import StatsGrid from "../../components/dashboard/StatsGrid";
+import SalesChart from "../../components/charts/SalesCharts";
 
 export default function DashboardPage() {
   return (
     <div className="space-y-6">
       <h1 className="text-3xl font-bold">Dashboard Overview</h1>
       <StatsGrid />
+      <SalesChart />
     </div>
   );
 }
