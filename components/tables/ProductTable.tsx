c1462c7ef555bad5427c36237a340050b96c5e8a
Kenny <oluyoleprovidence@gmail.com>
2026-06-26 09:56:11 +0100

    modified:   components/tables/ProductTable.tsx

--- components/tables/ProductTable.tsx
+++ components/tables/ProductTable.tsx
@@ -7,26 +7,25 @@ export default function ProductTable() {
       <table className="w-full">
         <thead>
           <tr className="border-b text-left">
-            <th className="pb-3">Name</th>
-            <th className="pb-3">Category</th>
-            <th className="pb-3">Price</th>
-            <th className="pb-3">Stock</th>
-            <th className="pb-3">Status</th>
+            <th className=" px-4 py-3">Name</th>
+            <th className="px-4 py-3">Category</th>
+            <th className="px-4 py-3">Stock</th>
+            <th className="px-4 py-3">Status</th>
           </tr>
         </thead>
 
         <tbody>
           {products.map((product) => (
             <tr key={product.id} className="border-b">
-              <td className="py-3">{product.name}</td>
+              <td className="px-4 py-3">{product.name}</td>
 
-              <td className="py-3">{product.category}</td>
+              <td className="px-4 py-3">{product.category}</td>
 
-              <td className="py-3">${product.price}</td>
+              <td className="px-4 py-3">${product.price}</td>
 
-              <td className="py-3">{product.stock}</td>
+              <td className="px-4 py-3">{product.stock}</td>
 
-              <td className="py-3">
+              <td className="px-4 py-3">
                 <StockBadge stock={product.stock} />
               </td>
             </tr>
